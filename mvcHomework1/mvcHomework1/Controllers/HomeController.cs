1d13a44e960a1e1b9b43e6ca968d85c239a24910
Max Huang <morphia1115@gmail.com>
2018-01-16 23:22:10 +0800

    Split history record to partial view and generate fake data into view through ViewModel

--- mvcHomework1/mvcHomework1/Controllers/HomeController.cs
+++ mvcHomework1/mvcHomework1/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using mvcHomework1.ViewModels;
 
 namespace mvcHomework1.Controllers
 {
@@ -10,7 +11,32 @@ namespace mvcHomework1.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            List<string> myFakeIncomeExpense = new List<string>();
+            List<DateTime> myFakePostTime = new List<DateTime>();
+            List<int> myFakeAmount = new List<int>();
+            Random random = new Random();
+
+            for(int i=1; i<=100; i++)
+            {
+                if (random.Next(2) == 1)
+                {
+                    myFakeIncomeExpense.Add("收入");
+                }
+                else
+                {
+                    myFakeIncomeExpense.Add("支出");
+                }
+
+                myFakePostTime.Add(DateTime.Now.AddDays(i-100));
+                myFakeAmount.Add(random.Next(10000));
+            }
+
+            MoenyRecordViewModel viewModel = new MoenyRecordViewModel();
+            viewModel.IncomeExpense = myFakeIncomeExpense;
+            viewModel.PostTime = myFakePostTime;
+            viewModel.Amount = myFakeAmount; 
+
+            return View(viewModel);
         }
 
         public ActionResult About()
---
+++ mvcHomework1/mvcHomework1/ViewModels/MoenyRecordViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mvcHomework1.ViewModels
+{
+    public class MoenyRecordViewModel
+    {
+        public List<string> IncomeExpense { get; set; }
+        public List<DateTime> PostTime { get; set; }
+        public List<int> Amount { get; set; }
+    }
+}
