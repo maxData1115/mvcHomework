ec8e66a9a66dc7ec58b4ab272500d95c3b4ab57d
Max Huang <morphia1115@gmail.com>
2018-01-24 20:00:32 +0800

    Build domain & repository model and then bind them by Dapper

---
+++ mvcHomework1/mvcHomework1/Models/Domain/AccountBooks.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mvcHomework1.Models.Domain
+{
+    public class AccountBooks
+    {
+        public Guid Id { get; set; }
+        public int Categoryyy { get; set; }
+        public int Amounttt { get; set; }
+        public DateTime Dateee { get; set; }
+        public string Remarkkk { get; set; }
+    }
+}
---
+++ mvcHomework1/mvcHomework1/Models/Repository/AccountBooksRepository.cs
@@ -0,0 +1,37 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using mvcHomework1.Models.Domain;
+
+namespace mvcHomework1.Models.Repository
+{
+    public class AccountBooksRepository
+    {
+        private string connectionString = ConfigurationManager.ConnectionStrings["HomeWorkDB"].ConnectionString;
+        public IEnumerable<AccountBooks> GetAccount()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("SELECT *");
+            sb.AppendLine("FROM AccountBook ab");
+            sb.AppendLine("WHERE 1=1");
+
+            try
+            {              
+                using (SqlConnection conn = new SqlConnection(this.connectionString))
+                {
+                    return conn.Query<AccountBooks>(sb.ToString());
+                }
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("GetAccount(): " + ex.Message);
+            }
+        }
+    }
+}
