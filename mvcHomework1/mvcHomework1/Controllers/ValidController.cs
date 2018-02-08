101187afaf13f56687f6d01700ac8b420a92df83
Max Huang <morphia1115@gmail.com>
2018-02-08 22:47:03 +0800

    加入日期 (不得大於今日/remote)與金額(限定輸入正整數/regex)的限制

---
+++ mvcHomework1/mvcHomework1/Controllers/ValidController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcHomework1.Controllers
+{
+    public class ValidController : Controller
+    {
+        // GET: Valid
+        public ActionResult Index(DateTime postTime)
+        {
+            bool isvalidat = postTime <= DateTime.Now;
+            return Json(isvalidat, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
--- mvcHomework1/mvcHomework1/ViewModels/MoneyRecordViewModel.cs
+++ mvcHomework1/mvcHomework1/ViewModels/MoneyRecordViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 using mvcHomework1.Enum;
 using Microsoft.SqlServer.Server;
 
@@ -15,11 +16,13 @@ namespace mvcHomework1.ViewModels
 
         [Required]
         [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}")]
+        [Remote("Index","Valid",ErrorMessage = "不得輸入今日以後之日期")]
         public DateTime PostTime { get; set; }
 
         [Required]
+        [RegularExpression(@"^\+?[1-9][0-9]*$",ErrorMessage = "限定輸入正整數")]
         [DisplayFormat(DataFormatString = "{0:#,#}")]
-        public Decimal Amount { get; set; }
+        public decimal Amount { get; set; }
 
         [Required]
         [StringLength(100)]
